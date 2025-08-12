aba40e8c963628dd545d9d2dc552b3c250b8d2a9
Thilakroshan <thilakroshan04@gmail.com>
2025-08-12 20:57:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+window.onload = () => {
+    const button = document.querySelector('#btn');
+    button.addEventListener('click' , calculateBmi)
+}
+
+function calculateBmi() {
+    const height = document.querySelector('#height').value;
+    const weight = document.querySelector('#weight').value;
+    const weight = document.querySelector('#result'
+
+    if(!height || isNaN(height) || height < 0){
+
+    }
+}
